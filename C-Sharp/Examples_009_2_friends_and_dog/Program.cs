ad5071f8ff456c0f5b7b6c3f372482ee791315d8
InfSub <inf.sub+github.com@gmail.com>
2022-12-15 23:01:38 +0300

    C# Examples 010 - square number

---
+++ C-Sharp/Examples_009_2_friends_and_dog/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;
+double distance = 10000;
+double first_friend_speed = 1; 
+double second_friend_speed = 2; 
+double dog_speed = 5;
+int friend = 2;
+double time = 0;
+
+while(distance > 10){
+    if(friend == 1){
+        time = distance / (first_friend_speed + dog_speed);
+        friend = 2;
+    }else{
+        time = distance / (second_friend_speed + dog_speed);
+        friend = 1;
+    };
+    distance = distance - (first_friend_speed + second_friend_speed) * time;
+    count++;    
+    /*
+    Console.WriteLine("count: "+ count);
+    Console.WriteLine("distance: "+ distance);
+    Console.WriteLine("time: "+ time);
+    Console.WriteLine("");
+    */
+};
+Console.WriteLine(count);
---
+++ C-Sharp/Examples_010_square_number/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+// variation 1
+// int a = int.Parse(Console.ReadLine());
+
+// variation 2
+int a = Convert.ToInt32(Console.ReadLine());
+
+// print
+// Console.WriteLine("Квадрат "+ a +" = "+ (a * a));
+
+// Интерполяция
+Console.WriteLine($"Квадрат {a}  = {a * a}");
+
