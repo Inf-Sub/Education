93d34c75141b18dcd8b65bb586025d09fa07ea50
InfSub <inf.sub+github.com@gmail.com>
2022-12-17 01:03:03 +0300

    Added solution to problem 8 from homework

---
+++ C-Sharp/Seminars/01_HomeWork/01_08_HomeWork/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Возможности:
+- Вывод чётных положительных и отрицательных чисел от 1 до N;
+- сортировка чисел, как положительных, так и отрицательных, как в задаче от 1 до N
+
+*/
+Console.WriteLine(
+    "Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.\n"
+);
+
+Console.Write("Введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int min = (1 < N) ? 1 : N;
+int max = (1 > N) ? 1 : N;
+string positive = "";
+string negative = "";
+
+if (min % 2 != 0)
+{
+    min++;
+};
+
+while (min <= max)
+{
+    positive += $"{min} ";
+    negative = $"{min} {negative} ";
+    min += 2;
+};
+
+if (max == N){
+    Console.WriteLine(positive);
+}else{
+    Console.WriteLine(negative);
+};
+
+
+
+
--- C-Sharp/Seminars/01_Work/01_05/Program.cs
+++ C-Sharp/Seminars/01_Work/01_05/Program.cs
@@ -1,5 +1,7 @@
-﻿int[ , ] digits = { {25, 5}, { 2, 10 }, {9, -3}, {-3, 9} };
+﻿/* 
+int[,] digits = { { 25, 5 }, { 2, 10 }, { 9, -3 }, { -3, 9 } };
 foreach (int i in digits)
 {
     Console.WriteLine(i);
-}
+} 
+*/
