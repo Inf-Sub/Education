fa35d185a9267b61e52424d0f03cfe3939bc0e5b
InfSub <inf.sub+github.com@gmail.com>
2022-12-17 04:15:34 +0300

    added solutions to problems 5 and 7 from homework

--- C-Sharp/Seminars/01_Work/01_07/Program.cs
+++ C-Sharp/Seminars/01_Work/01_07/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Итерация - повторение какого-либо действия
+
+Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+	456 -> 6
+	782 -> 2
+	918 -> 8
+
+*/
+
+while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine(
+        "Задача №7. Напишите программу, которая принимает на вход трёхзначное число\n" +
+        "и на выходе показывает последнюю цифру этого числа.\n"
+    );
+
+    Console.Write("Введите целое трехзначное число: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    if (a < 100 || a > 999)
+    {
+        Console.WriteLine($"Вы ввели НЕ трехзначное число, ваше число: {a}. Попробуйте еще раз!");
+        System.Threading.Thread.Sleep(2000);
+    }
+    else
+    {
+        Console.WriteLine($"{a % 10}");
+        break;
+    };
+};
