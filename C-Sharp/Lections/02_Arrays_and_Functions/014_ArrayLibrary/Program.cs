84712b963d618c145e3747cef6837cb16bb16364
InfSub <inf.sub+github.com@gmail.com>
2022-12-19 02:16:12 +0300

    C#: Lection 02: 014_ArrayLibrary: ArrayLibraly program is over

--- C-Sharp/Lections/02_Arrays_and_Functions/014_ArrayLibrary/Program.cs
+++ C-Sharp/Lections/02_Arrays_and_Functions/014_ArrayLibrary/Program.cs
@@ -1,1 +1,66 @@
-﻿
+﻿// create Method (Function) FillArray with random int
+void FillArray(int[] collecton)
+{
+    int len = collecton.Length;
+    int index = 0;
+    while (index < len)
+    {
+        collecton[index] = new Random().Next(1, 10);
+        index++;
+    };
+};
+
+// create Method (Function) PrintArray to Console
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int key = 0;
+
+    Console.Write($"Array: [ ");
+    while (key < count)
+    {
+        Console.Write($"{array[key]} ");
+        key++;
+    };
+    Console.WriteLine($"]");
+};
+
+// Method with return
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        };
+        index++;
+    };
+
+    //Console.WriteLine($"{position}");
+    return position;
+};
+
+
+Console.Write($"Enter a number from 1 to 9 to search the array: ");
+int find = Convert.ToInt32(Console.ReadLine());
+// создаем массив на 10 целых чисел
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+int pos = IndexOf(array, find);
+
+if (pos >= 0)
+{
+    Console.WriteLine($"Number: >> {find} << found at position: >> {pos} << of the array");
+}
+else
+{
+    Console.WriteLine($"Number: >> {find} << NOT found of the array");
+};
