6fb7a6d3540b3305497b7e3b4573bcbe630dc869
InfSub <inf.sub+github.com@gmail.com>
2022-12-18 23:32:11 +0300

    Lection 2 Function and Array: ArrayLibrary and rename old projects, and added project 010_findMax (without function and array)

---
+++ C-Sharp/Lections/02_Arrays_and_Functions/010_findMax/Program.cs
@@ -0,0 +1,25 @@
+﻿// version 1
+
+int a1 = 43;
+int a2 = 23;
+int a3 = 33;
+int b1 = 85;
+int b2 = 58;
+int b3 = 15;
+int c1 = 48;
+int c2 = 78;
+int c3 = 34;
+
+int max = a1;
+if(b1 > max) max = b1;
+if(c1 > max) max = c1;
+
+if(a2 > max) max = a2;
+if(b2 > max) max = b2;
+if(c2 > max) max = c2;
+
+if(a3 > max) max = a3;
+if(b3 > max) max = b3;
+if(c3 > max) max = c3;
+
+Console.WriteLine(max);
---
+++ C-Sharp/Lections/02_Arrays_and_Functions/013_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {43, 23, 33, 54, 58, 115, 33, 78, 34};
+
+int len = array.Length;
+int find = 33;
+
+int index = 0;
+
+while(index < len)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    };
+    index++;
+};
