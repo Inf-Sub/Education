cffc722e582e7f99ab4b79df3ab6d95a77b51840
InfSub <inf.sub+github.com@gmail.com>
2022-12-19 21:21:46 +0300

    added C# 02 Seminar task 12

--- C-Sharp/Seminars/02_Work/02_12/Program.cs
+++ C-Sharp/Seminars/02_Work/02_12/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача №12. Работа в группах 
+Напишите программу, которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+34, 5 -> не кратно, остаток 4
+16, 4 -> кратно
+*/
+
+char[] charsToTrim = {' '};
+
+double ReadDoubleFromString(string write){
+    Console.Write($"{write}");
+    string? text = Console.ReadLine()?.Trim(charsToTrim).Replace(".", ",");
+
+    return Convert.ToDouble(text);
+};
+
+
+Console.Clear();
+
+Console.WriteLine(
+    $"Задача №12. Напишите программу, которая будет принимать на вход "+
+    $"два числа и выводить, является ли второе число кратным первому."+
+    $"Если число 2 не кратно числу 1, то программа выводит остаток от деления.\n"
+);
+
+double first = ReadDoubleFromString("Введите первое число: ");
+double second = ReadDoubleFromString("Введите второе число: ");
+
+//int first = new Random().Next(1, 1000);
+//int second = new Random().Next(1, 1000);
+
+if(first % second == 0){
+    Console.WriteLine($"Число {first} кратно {second}");
+}else{
+    Console.WriteLine($"Число {first} НЕ кратно {second}, остаток от деления: {first % second}");
+};
