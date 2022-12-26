f59950031c41b276cfc447964477d4031e2451b4
InfSub <inf.sub+github.com@gmail.com>
2022-12-26 05:02:18 +0300

    C#: Seminar 3: HomeWork: added task 13

--- C-Sharp/Seminars/02/02_HomeWork/hw_s02_t13/Program.cs
+++ C-Sharp/Seminars/02/02_HomeWork/hw_s02_t13/Program.cs
@@ -1,4 +1,49 @@
 ﻿/*
 Задача 13:
 Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
 */
+
+int num;
+int module;
+int typeTask = 0;
+
+void WaitSeconds(double sec)
+{
+    System.Threading.Thread.Sleep((int)(sec * 1000));
+};
+
+
+while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine(
+        "Задача 13:\n"+
+        "Напишите программу, которая выводит третью цифру заданного\n"+
+        "числа или сообщает, что третьей цифры нет.\n"
+    );
+
+    Console.WriteLine(
+        "Press Ctrl+C for Exit.\n"
+    );
+
+    if(typeTask == 0){
+        Console.Write("Введите 1 для ручного ввода числа или 2 для генерации рандомного числа: ");
+        typeTask = Convert.ToInt32(Console.ReadLine());
+    };
+
+    if(typeTask == 1){
+        Console.Write("Введите целое число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+    }else{
+        num = new Random().Next(-10000, 10000);
+    };
+    module = (num >= 0) ? num : num * -1;
+
+    Console.WriteLine($"{num} => {module / 100 % 10}");
+    WaitSeconds(2);
+};
