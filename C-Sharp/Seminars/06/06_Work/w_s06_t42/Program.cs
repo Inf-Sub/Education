8406252dfb93d7b15543d8e882222f2ef13f7db2
InfSub <inf.sub+github.com@gmail.com>
2023-01-20 04:55:22 +0300

    C sharp: Seminar 6: assignment 42, 44 - solved (update task 42)

--- C-Sharp/Seminars/06/06_Work/w_s06_t42/Program.cs
+++ C-Sharp/Seminars/06/06_Work/w_s06_t42/Program.cs
@@ -9,14 +9,14 @@
 
 
 
-int num = new Random().Next(1, 1000);
-num = 856;
-int result = from10to2(num);
+int num = new Random().Next(1, 100000);
+//num = 856;
+long result = from10to2(num);
 
 Console.WriteLine($"num: {num} -> result: {result}");
 
 
-int from10to2(int num){
+long from10to2(int num){
     string result = String.Empty;
     while(num > 0){
         Console.Write($"num: {num} / 2 = ");
@@ -24,7 +24,7 @@ int from10to2(int num){
         num /= 2;
         Console.WriteLine($"{num} result: {result}; ");
     };
-    return Convert.ToInt32(result);
+    return long.Parse(result);
 };
 
 
@@ -46,22 +46,24 @@ int from10to2(int num){
 //Console.Write($"Введите число в десятичном формате для преобразование в двоичное: ");
 int N = num;
 //int N = Convert.ToInt32(Console.ReadLine());
-int[] Array = CreateRandomArray(Convert.ToInt32(Math.Log2(N))+1, 0, 0);
+//int[] Array = CreateRandomArray(Convert.ToInt32(Math.Log2(N))+1, 0, 0);
+int[] Array = new int[Convert.ToInt32(Math.Log2(N))+1];
 int i = 0;
 //Console.WriteLine($"{Convert.ToInt32(Math.Log2(N))} {Convert.ToInt32(Math.Log2(N))+1}");
-ShowArray(Array);
+//ShowArray(Array);
 while (N > 0)
 {
     Array[Array.Length-i-1] = N % 2;
     Console.Write($"i: {i} N: {N} / 2 = ");
     N = N/2;
-    Console.WriteLine($"{N} result: {Array[Array.Length-i-1]}");
+    Console.WriteLine($"{N} result: {Array[Array.Length-i-1]} {long.Parse(String.Join("", Array))}");
     i++;
 }
+Console.WriteLine($"num: {num}");
 ShowArray(Array);
 
 
-
+/*
 int[] CreateRandomArray(int size, int min, int max)
 {
     int[] array = new int[size];
@@ -71,6 +73,7 @@ int[] CreateRandomArray(int size, int min, int max)
     }
     return array;
 };
+*/
 void ShowArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
