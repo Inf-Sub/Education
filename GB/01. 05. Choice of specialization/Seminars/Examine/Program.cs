ee5ea5564882503b9b38d56580b07903a0e540ad
InfSub <inf.sub+github.com@gmail.com>
2023-02-12 20:47:29 +0300

    project Examine: added method ViewTitleTask()

--- GB/01. 05. Choice of specialization/Seminars/Examine/Program.cs
+++ GB/01. 05. Choice of specialization/Seminars/Examine/Program.cs
@@ -7,7 +7,23 @@ void Run(){
     string title =  $"";
         
 
-
+    ViewTitleTask(title);
     Console.Clear();
     Console.WriteLine(); 
+};
+
+
+
+
+// ViewTitleTask version 2.1
+void ViewTitleTask(string? text = ""){
+    string rn = "\n";
+    Console.Clear();
+    Console.WriteLine(
+        $"{text}{rn}"+
+        $"Для завершения программы, нажмите Ctrl + C{rn}{rn}"+
+        $"Press any key to continue..."
+    );
+    Console.ReadKey();
+    Console.Clear();
 };
