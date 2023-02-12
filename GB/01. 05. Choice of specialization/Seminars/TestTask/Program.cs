af31d6c4a541ec0f8ea8a86b8f4a20715b3b26ef
InfSub <inf.sub+github.com@gmail.com>
2023-02-13 02:10:19 +0300

    added method ViewTitleTask()

--- GB/01. 05. Choice of specialization/Seminars/TestTask/Program.cs
+++ GB/01. 05. Choice of specialization/Seminars/TestTask/Program.cs
@@ -4,11 +4,36 @@ Run();
 
 void Run(){
     string rn = "\n";
-    string title = "";
+    string title = 
+        $"Контрольная работа.{rn}"+
+        $"Задача:{rn}"+
+        $"Напишите программу, которая из имеющегося массива строк формирует массив из строк,{rn}"+
+        $"длина которых меньше либо равна 3 символам.{rn}"+
+        $"{rn}"+
+        $"Например:{rn}"+
+        $"['hello', '2', 'world', ':-)'] -> ['2', ':-)']{rn}"+
+        $"['1234', '1567', '-2', 'computer science'] -> ['-2']{rn}"+
+        $"['Russia', 'Denmark', 'Kazan'] -> []{rn}";
+
+
+    ViewTitleTask(title);
     
     Console.Clear();
+
 
     Console.WriteLine(); 
 };
 
 
+// ViewTitleTask version 2.1
+void ViewTitleTask(string? text = ""){
+    string rn = "\n";
+    Console.Clear();
+    Console.WriteLine(
+        $"{text}{rn}"+
+        $"Для завершения программы, нажмите Ctrl + C{rn}{rn}"+
+        $"Press any key to continue..."
+    );
+    Console.ReadKey();
+    Console.Clear();
+};
